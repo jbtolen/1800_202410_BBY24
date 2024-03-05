ee965479301ab6ba4e9dd36dfe28ba3eecdd7a40
James Tolen <jtolen1@my.bcit.ca>
2024-03-05 11:00:35 -0800

    created a JS file and added API key

---
+++ scripts/firebaseAPI_TEAM24.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCyYAihelFQgwpc_ZjYoFS-M_u_BSGtAPI",
+    authDomain: "comp1800-bby24.firebaseapp.com",
+    projectId: "comp1800-bby24",
+    storageBucket: "comp1800-bby24.appspot.com",
+    messagingSenderId: "238450251631",
+    appId: "1:238450251631:web:f7309e9bee1b6d755d3a9a"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
