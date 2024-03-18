5d84ecae38ecdfe9e1641d223004898c7be8b685
James Tolen <jtolen1@my.bcit.ca>
2024-03-18 13:16:00 -0700

    removed quotes function

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,5 @@
 // Function to read the quote of the day from the Firestore "quotes" collection
 // Input param is the String representing the day of the week, aka, the document name
-function readQuote(day) {
-    db.collection("quotes").doc(day)                                                      //name of the collection and documents should matach excatly with what you have in Firestore
-      .onSnapshot(dayDoc => {                                                               //arrow notation
-           console.log("current document data: " + dayDoc.data());                          //.data() returns data object
-           document.getElementById("quote-goes-here").innerHTML = dayDoc.data().quote;      //using javascript to display the data on the right place
-           
-           //Here are other ways to access key-value data fields
-           //$('#quote-goes-here').text(dayDoc.data().quote);         //using jquery object dot notation
-           //$("#quote-goes-here").text(dayDoc.data()["quote"]);      //using json object indexing
-		       //document.querySelector("#quote-goes-here").innerHTML = dayDoc.data().quote;
-      })
-}
-readQuote("none");        //calling the function
 
 // Wait for the DOM to load
 document.addEventListener("DOMContentLoaded", function() {
@@ -42,7 +29,7 @@ function updateCollection() {
 
     if (user) {
         var userId = user.uid;
-        var collectionRef = db.collection("users").doc(userId).collection("your_collection");
+        var collectionRef = db.collection("users").doc(userId).collection("Water Added");
 
         // Update collection data
         // For example, you can add a new document to the collection
