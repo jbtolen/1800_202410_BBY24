b4f1731766a8d6f17664ae923f8ab7ba5561b38a
James Tolen <jtolen1@my.bcit.ca>
2024-04-01 23:41:58 -0700

    created a popup for the user to input a goal when first logging in and added a add customer amount function

--- scripts/main.js
+++ scripts/main.js
@@ -1,221 +1,275 @@
+// Wait for the DOM content to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+    populateUserGoal(); // Populate user's goal
+    updateCollection(0); // Update collection with default option (0)
 
-document.addEventListener("DOMContentLoaded", function() {
+    // Button references
+    var buttons = [
+        document.getElementById('amount1'),
+        document.getElementById('amount2'),
+        document.getElementById('amount3'),
+        document.getElementById('reset')
+    ];
 
-    // run the function once to display the initial amount upon page load
-    // need to somehow have code here that retrives and display user current drank amount
-
-    updateCollection(0);
-    
-    // Get reference to the button
-    var updateCollectionButton1 = document.getElementById('amount1');
-    var updateCollectionButton2 = document.getElementById('amount2');
-    var updateCollectionButton3 = document.getElementById('amount3');
-    var updateCollectionButtonReset = document.getElementById('reset');
-
-    // Add event listener to the button
-    updateCollectionButton1.addEventListener('click', function() {
-        updateCollection(1);
-        console.log("button 1 clicked");
+    // Add event listeners to buttons
+    buttons.forEach((button, index) => {
+        button.addEventListener('click', function () {
+            updateCollection(index + 1); // Update collection based on button index
+            console.log("button", index + 1, "clicked");
+        });
     });
-    updateCollectionButton2.addEventListener('click', function() {
-        updateCollection(2);
-        console.log("button 2 clicked");
-    });
-    updateCollectionButton3.addEventListener('click', function() {
-        updateCollection(3);
-        console.log("button 3 clicked");
-    });
-
-    updateCollectionButtonReset.addEventListener('click', function(){
-        updateCollection(4);
-        console.log("button reset clicked");
-    });
-
-    var totalAmountDisplay = document.querySelector('.display-5.fw-bold.text-body-emphasis');
-    var userGoal = fetchUserGoal();
-    if (userGoal) {
-        totalAmountDisplay.textContent += " / Goal: " + userGoal + "oz";
-    }
-
 });
 
-
-
-// function logInUponPageLoad() {
-//     firebase.auth().onAuthStateChanged(function(user) {
-//         if (user) {
-//             console.log("User signed in:", user);
-//             return user;
-//         } else {
-//             console.log("No user signed in.");
-//         }
-//     });
-// }
-
-function updateCollection(option, currentUser) {
-    // var user = firebase.auth().currentUser
+// Update collection based on buttons or customer amount
+function updateCollection(option) {
     var keyToAdd;
-    console.log("entered updateCollection function");
-
-    var userGoal = fetchUserGoal();
-
+    var currentDate = new Date().toISOString().split('T')[0]; // Get current date
 
     switch (option) {
         case 1:
             keyToAdd = 14;
-            console.log("key changed to 14");
             break;
         case 2:
             keyToAdd = 22;
-            console.log("key changed to 23");
             break;
         case 3:
             keyToAdd = 32;
-            console.log("key changed to 32");
             break;
         case 4:
+            keyToAdd = 0; // Reset to 0 when reset button is clicked
             console.log("key reset to 0");
             break;
-            //option used for just display the result initially
         case 0:
             keyToAdd = 0;
-            console.log("key = 0");
             break;
         default:
-            console.error("Invalid option:", option);
-            return; // Exit the function if the option is invalid
+            if (option === 0) {
+                keyToAdd = 0;
+            } else {
+                // Handle custom amount here
+                keyToAdd = option;
+            }
+            break;
     }
 
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            var userId = user.uid;
+            var docRef = db.collection("users").doc(userId).collection("your_collection").doc(currentDate);
 
-//saves data to one document
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        console.log("User logged in");
-        var userId = user.uid;
-        // Returns the string of the date and puts the format in YYYY-MM-DDTHH
-        var currentDate = new Date().toISOString().split('T')[0]; // Get current date
-        var docRef = db.collection("users").doc(userId).collection("your_collection").doc(currentDate);
- 
-        // Check if the document for the current date exists
-        docRef.get()
-        .then(function(doc) {
-            if (doc.exists) {
-                var currentValue = doc.data().key || 0;
-                var updatedValue = currentValue + keyToAdd;
+            // Check if the document for the current date exists
+            docRef.get()
+                .then(function (doc) {
+                    var currentValue = doc.exists ? doc.data().key || 0 : 0;
+                    var updatedValue = (option === 4 || !doc.exists) ? 0 : currentValue + keyToAdd;
 
-                console.log("Existing value:", currentValue);
-                console.log("New value:", updatedValue);
-
-                // Update document data
-                docRef.update({
-                    key: updatedValue
+                    docRef.set({ key: updatedValue })
+                        .then(function () {
+                            console.log(doc.exists ? "Document updated" : "Document created", "successfully");
+                            fetchTotalAmount();
+                        })
+                        .catch(function (error) {
+                            console.error("Error updating document:", error);
+                        });
                 })
-                .then(function() {
-                    console.log("Document updated successfully");
-                    
-                    fetchTotalAmount()
-                    .then(function(totalAmount) {
-                        var totalAmountDisplay = document.querySelector('.display-5.fw-bold.text-body-emphasis');
-                        totalAmountDisplay.textContent = "You drank a total of " + totalAmount + "oz" + "/";
-                    })
-                    .catch(function(error) {
-                        console.error("Error fetching total amount:", error);
-                    });
-                })
-                .catch(function(error) {
-                    console.error("Error updating document: ", error);
+                .catch(function (error) {
+                    console.error("Error getting document:", error);
                 });
-            } else {
-                // If the document doesn't exist, create a new one with the provided keyToAdd value
-                docRef.set({
-                    key: keyToAdd
-                })
-                .then(function() {
-                    console.log("Document created successfully");
-
-                    fetchTotalAmount()
-                    .then(function(totalAmount) {
-                        var totalAmountDisplay = document.querySelector('.display-5.fw-bold.text-body-emphasis');
-                        totalAmountDisplay.textContent = "You drank a total of " + totalAmount + "oz";
-                    })
-                    .catch(function(error) {
-                        console.error("Error fetching total amount:", error);
-                    });
-                })
-                .catch(function(error) {
-                    console.error("Error creating document: ", error);
-                });
-            }
-        })
-        .catch(function(error) {
-            console.error("Error getting document:", error);
-        });
-    } else {
-        console.log("No user signed in.");
-    }
-});
-
-}
-
-
-
-function fetchTotalAmount() {
-    return new Promise(function(resolve, reject) {
-
-        firebase.auth().onAuthStateChanged(function(user) {
-            if (user) {
-                var userId = user.uid;
-                var collectionRef = db.collection("users").doc(userId).collection("your_collection");
-
-                collectionRef.get()
-                .then(function(querySnapshot) {
-                    var totalAmount = 0;
-                    querySnapshot.forEach(function(doc) {
-                        // Sum up the values of all documents
-                        totalAmount += doc.data().key;
-                    });
-                    resolve(totalAmount);
-                })
-                .catch(function(error) {
-                    reject(error);
-                });
-            } else {
-                reject("No user signed in.");
-            }
-        });
+        } else {
+            console.log("No user signed in.");
+        }
     });
 }
 
-// function displayQuote(day){
-//     db.collection("quotes").doc(day)
-//         .onSnapshot(dayDoc => {
-//             console.log("current document data: " + dayDoc.data());
-//             document.getElementById("daily-quote").innerHTML = dayDoc.data().quote;
-//         })
-// }
-// displayQuote("tuesday");
-
-// document.getElementById("inputForm").addEventListener("submit", function(event) {
-
-//     var inputElement = document.getElementById("goalInput");
-//     var inputValue = inputElement.value;
-//     document.getElementById("displayArea").innerText = "The input number is: " + inputValue;
-// });
-
-function fetchUserGoal() {
-    var user = firebase.auth().currentUser;
-    if (user) {
-        var userId = user.uid;
-        var userDoc = db.collection("users").doc(userId);
-        var doc = userDoc.get();
-        if (doc.exists) {
-            return doc.data().goal;
+// Wait for the DOM content to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+    // Check if the user is logged in
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // Check if the user's goal exists
+            checkUserGoal(user);
         } else {
-            console.error("User document not found");
-            return null;
+            console.log("No user signed in.");
         }
+    });
+});
+
+// Function to check if the user's goal exists
+function checkUserGoal(user) {
+    var userId = user.uid;
+    var userRef = db.collection("users").doc(userId);
+
+    userRef.get().then(function (doc) {
+        if (doc.exists) {
+            var userData = doc.data();
+            var userGoal = userData.goal;
+
+            if (userGoal) {
+                // User's goal exists, proceed with other functionality
+                populateUserGoal();
+                updateCollection(0);
+            } else {
+                // User's goal doesn't exist, prompt for input
+                promptUserGoal(userRef);
+            }
+        } else {
+            console.log("No such document!");
+        }
+    }).catch(function (error) {
+        console.log("Error getting document:", error);
+    });
+}
+
+// Function to prompt the user to input their goal
+function promptUserGoal(userRef) {
+    var goalInput = prompt("Please enter your goal:");
+
+    if (goalInput !== null && goalInput.trim() !== "") {
+        // Save the goal to the database
+        saveUserGoal(userRef, goalInput.trim());
     } else {
-        console.error("No user signed in.");
-        return null;
+        console.log("Goal input is empty or canceled.");
+    }
+}
+
+function saveUserGoal(userRef, goal) {
+    userRef.update({
+        goal: goal
+    }).then(function () {
+        console.log("User's goal successfully saved:", goal);
+        // Proceed with other functionality after saving the goal
+        populateUserGoal();
+        updateCollection(0);
+    }).catch(function (error) {
+        console.error("Error updating document:", error);
+    });
+}
+
+// Populate user's goal
+function populateUserGoal() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            var currentUser = db.collection("users").doc(user.uid);
+            currentUser.get()
+                .then(function (userDoc) {
+                    var userGoal = userDoc.data()?.goal;
+                    if (userGoal != null) {
+                        document.getElementById("goalInput").value = userGoal;
+                    }
+                    fetchTotalAmount();
+                })
+                .catch(function (error) {
+                    console.error("Error getting user document: ", error);
+                });
+        } else {
+            console.log("No user is signed in");
+        }
+    });
+}
+// Fetch total amount from database
+function fetchTotalAmount() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            var userId = user.uid;
+            var collectionRef = db.collection("users").doc(userId).collection("your_collection");
+
+            collectionRef.get()
+                .then(function (querySnapshot) {
+                    var totalAmount = 0;
+                    querySnapshot.forEach(function (doc) {
+                        totalAmount += parseInt(doc.data().key || 0);
+                    });
+                    updateTotalAmountDisplay(totalAmount);
+                })
+                .catch(function (error) {
+                    console.error("Error getting documents: ", error);
+                });
+        } else {
+            console.log("No user signed in.");
+        }
+    });
+}
+
+// Update total amount display
+function updateTotalAmountDisplay(currentValue) {
+    var userGoal = parseInt(document.getElementById('goalInput').value);
+    var totalAmountDisplay = document.querySelector('.display-5.fw-bold.text-body-emphasis');
+    var message = (currentValue === 0) ? `You drank a total of 0/${userGoal}oz` : `You drank a total of ${currentValue}/${userGoal}oz`;
+    if (totalAmountDisplay) {
+        totalAmountDisplay.textContent = message;
+    } else {
+        console.error("Total amount display element not found.");
+    }
+}
+
+
+
+
+//call the function to run it 
+function editUserInfo() {
+    //Enable the form fields
+    document.getElementById('personalInfoFields').disabled = false;
+}
+
+function saveUserInfo() {
+    var userGoal = parseInt(document.getElementById('goalInput').value); // Get the user's goal from the input field
+    console.log("User Goal:", userGoal); // Logging userGoal value
+
+    var user = firebase.auth().currentUser; // Fetch the current user
+
+    if (user) {
+        var currentUser = db.collection("users").doc(user.uid);
+
+        currentUser.update({
+            goal: userGoal
+        })
+            .then(function () {
+                console.log("Document successfully updated with user goal:", userGoal);
+                populateUserGoal(); // Update the displayed user goal after saving
+                // Disable the form fields after saving
+                document.getElementById('personalInfoFields').disabled = true;
+            })
+            .catch(function (error) {
+                console.error("Error updating document:", error);
+            });
+    } else {
+        console.log("No user signed in.");
+    }
+}
+// Function to add custom amount
+function showPopup() {
+    document.getElementById('popup').style.display = 'block';
+    document.querySelector('.overlay').style.display = 'block';
+}
+
+function hidePopup() {
+    document.getElementById('popup').style.display = 'none';
+    document.querySelector('.overlay').style.display = 'none';
+}
+
+// Function to show popup with input field for custom amount
+function showPopup() {
+    // Display the popup and overlay
+    document.getElementById('popup').style.display = 'block';
+    document.querySelector('.overlay').style.display = 'block';
+
+    // Clear any previous input value
+    document.getElementById('customAmountInput').value = '';
+}
+
+// Function to add custom amount to the collection
+function addCustomAmount() {
+    // Get the custom amount entered by the user
+    var customAmount = parseInt(document.getElementById('customAmountInput').value);
+
+    // Check if the custom amount is valid
+    if (!isNaN(customAmount)) {
+        // Update the collection with the custom amount
+        updateCollection(customAmount); // Pass the custom amount to updateCollection()
+        // Hide the popup and overlay
+        hidePopup();
+    } else {
+        // Notify the user that the input is invalid
+        alert('Please enter a valid number for the custom amount.');
     }
 }
